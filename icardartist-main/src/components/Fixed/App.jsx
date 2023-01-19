8890e1c50e37f6691ff06ed9136b637c40bf1e76
KHUNTPRIYANSH <khuntpriyansh1@gmail.com>
2023-01-19 22:44:38 +0530

    cursor

--- icardartist-main/src/components/Fixed/App.jsx
+++ icardartist-main/src/components/Fixed/App.jsx
@@ -15,7 +15,32 @@ import { dividerClasses } from "@mui/material";
 import Home from "../Artist/Home/Home";
 import About from "../Artist/Home/About";
 import Contact from "../Artist/Home/Contact";
+import LegendaryCursor from "legendary-cursor";
 
+window.addEventListener("load", () => {
+  LegendaryCursor.init({
+    lineSize: 0.05,
+    opacityDecrement: 0.55,
+    speedExpFactor: 0.8,
+    lineExpFactor: 0.6,
+    sparklesCount: 65,
+    maxOpacity: 0.99,
+
+    // lineSize: 0,
+    // opacityDecrement: 0,
+    // speedExpFactor: 0.9,
+    // lineExpFactor: 0,
+    // sparklesCount: 55,
+    // maxOpacity: 0.000001, // should be a number between [0 ... 1]
+    texture1:
+      "https://raw.githubusercontent.com/KHUNTPRIYANSH/site_photos/main/grad/Screenshot%20(426).png", // texture displayed on mouse hover
+    texture2:
+      "https://raw.githubusercontent.com/KHUNTPRIYANSH/site_photos/main/grad/Screenshot%20(426).png", // texture displayed on mouse hover
+    // texture2:         "http://path_to_texture",      // texture displayed on mouse click
+    texture3:
+      "https://raw.githubusercontent.com/KHUNTPRIYANSH/site_photos/main/grad/Screenshot%20(426).png", // texture displayed on sparkles
+  });
+});
 function App() {
   const [count, setCount] = useState(0);
 
@@ -31,7 +56,7 @@ function App() {
             element={
               <div id="art-site">
                 <Menu />
-                <div className="bbbbb">
+                <div className="bbbbb" id="cus">
                   <Home />
                   <About />
                 </div>
